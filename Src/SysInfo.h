d7ed4d1ea5bfcae67c1a5f76785a2759640db307
John_Xu <John_Xu@avision.com.tw>
2019-09-27 16:31:40 +0800

    fix:datatype for 64 bits compiler.

--- Src/SysInfo.h
+++ Src/SysInfo.h
@@ -2,64 +2,67 @@
 #define _SYSINFO_H__
 
 #ifndef WIN32
-	//using namespace std;		
-	typedef char				INT8;
-	typedef unsigned char		UINT8;
-	typedef unsigned char		BYTE;
+#include <stdint.h>
 
-	typedef short				INT16;
-	typedef unsigned short      UINT16;
-	typedef unsigned short      WORD;
+typedef int8_t		INT8;
+typedef uint8_t		UINT8;
+typedef uint8_t		BYTE;
 
-	typedef long				INT32;
-	typedef unsigned long       UINT32;
-	typedef unsigned long       DWORD;
+typedef int16_t		INT16;
+typedef uint16_t    UINT16;
+typedef uint16_t    WORD;
 
-	typedef long long			INT64;
-	typedef unsigned long long	UINT64;
-	typedef long long			LONGLONG;
-	typedef unsigned long long	ULONGLONG;
+#define ICUINT32TYPE //For Icc4.h icUInt32Number --> 64bits for 64bit compiler
+typedef int32_t		INT32;
+typedef uint32_t    UINT32;
+typedef uint32_t    DWORD;
 
-	typedef unsigned int	 	UINT;
-	typedef char				INSTR;
-	typedef unsigned char		UINSTR;
-	typedef bool	 			BOOL;
-	typedef WORD				WINSTR;
-	typedef const WINSTR		LPCWSTR;
-	typedef BYTE *				LPBYTE;
-	typedef WORD *				LPWORD;
-	typedef DWORD *				LPDWORD;
-	typedef int*				LPINT;
-	typedef const char*			LPCSTR;
-	//typedef int					size_t;
-	
-	#define FALSE				0
-	#define TRUE				1
-	#define nullptr				0
+#define ICUINT64TYPE
+typedef int64_t		INT64;
+typedef int64_t		LONGLONG;
+typedef uint64_t	UINT64;
+typedef uint64_t	ULONGLONG;
 
-	#ifdef Mac
-	#define HiByteFirst
-	#endif 
+typedef unsigned int	 	UINT;
+typedef char				INSTR;
+typedef unsigned char		UINSTR;
+typedef bool	 			BOOL;
+typedef WORD				WINSTR;
+typedef const WINSTR		LPCWSTR;
+typedef BYTE *				LPBYTE;
+typedef WORD *				LPWORD;
+typedef DWORD *				LPDWORD;
+typedef int*				LPINT;
+typedef const char*			LPCSTR;
+//typedef int					size_t;
+
+#define FALSE				0
+#define TRUE				1
+#define nullptr				0
+
+#ifdef Mac
+#define HiByteFirst
+#endif 
 #else
-	#include <windows.h>
-	#include <stdlib.h>
-	#ifdef _DEBUG
-		#define DBG_Printf(fmt, ...)	printf("XUJY:"##fmt, ## __VA_ARGS__)
+#include <windows.h>
+#include <stdlib.h>
+#ifdef _DEBUG
+#define DBG_Printf(fmt, ...)	printf("XUJY:"##fmt, ## __VA_ARGS__)
 
-		//For Memory leak detection.
-		#define _CRTDBG_MAP_ALLOC 
-		#ifndef DBG_NEW     
-			#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )     
-		#endif//DBG_NEW
-		
-		#define new DBG_NEW 
-		#define DETECT_MEMORY_LEAKS _CrtDumpMemoryLeaks()
-		//You can using these command to find who alloc the momory.
-		//VC will pause when alloc memory 100 times.
-		//_CrtSetBreakAlloc(100) or _crtBreakAlloc = 100; 
-	#else		
-		#define DETECT_MEMORY_LEAKS 
-	#endif //_DEBUG
+//For Memory leak detection.
+#define _CRTDBG_MAP_ALLOC 
+#ifndef DBG_NEW     
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )     
+#endif//DBG_NEW
+
+#define new DBG_NEW 
+#define DETECT_MEMORY_LEAKS _CrtDumpMemoryLeaks()
+//You can using these command to find who alloc the momory.
+//VC will pause when alloc memory 100 times.
+//_CrtSetBreakAlloc(100) or _crtBreakAlloc = 100; 
+#else		
+#define DETECT_MEMORY_LEAKS 
+#endif //_DEBUG
 #endif//WIN32
 
 // *************************************************
@@ -70,34 +73,38 @@
 #define FileNotFound		0xFFFFFFFE
 
 #ifdef _DEBUG
-	#define DBG_Printf(fmt, ...)	printf("XUJY:"##fmt, ## __VA_ARGS__)
+#define DBG_Printf(fmt, ...)	printf("XUJY:"##fmt, ## __VA_ARGS__)
 #else
-	#define DBG_Printf(fmt, ...)	
+#define DBG_Printf(fmt, ...)	
 #endif //_DEBUG
 
 //DWORD SwapDWORD(const DWORD x);
 //WORD SwapWORD(const WORD x);
 
 #ifdef HiByteFirst 
-	inline DWORD SwapDWORD(const DWORD x)
-	{	return x;}
+inline DWORD SwapDWORD(const DWORD x)
+{
+	return x;
+}
 
-	inline WORD SwapWORD(const WORD x)
-	{	return x;}
+inline WORD SwapWORD(const WORD x)
+{
+	return x;
+}
 #else	
-	#define	SWAP
-	#ifdef WIN32  //For VC (Little Endian)		
-		#ifdef WIN64
-			#define SwapWORD _byteswap_ushort 
-			#define SwapDWORD _byteswap_ulong 
-		#else
-			#define SwapWORD _byteswap_ushort 
-			#define SwapDWORD _byteswap_ulong 
-		#endif //WIN64
-	#else	
-		#define SwapWORD __builtin_bswap16  
-		#define SwapDWORD __builtin_bswap32 
-	#endif//WIN32
+#define	SWAP
+#ifdef WIN32  //For VC (Little Endian)		
+#ifdef WIN64
+#define SwapWORD _byteswap_ushort 
+#define SwapDWORD _byteswap_ulong 
+#else
+#define SwapWORD _byteswap_ushort 
+#define SwapDWORD _byteswap_ulong 
+#endif //WIN64
+#else	
+#define SwapWORD __builtin_bswap16  
+#define SwapDWORD __builtin_bswap32 
+#endif//WIN32
 
 inline void SwapDWORD_Buf(LPDWORD lpBuf, int Size)
 {
@@ -120,13 +127,13 @@ inline void SwapWORD_Buf(LPWORD lpBuf, int Size)
 #define ENABLE_SHARED_POINTER
 
 #ifdef ENABLE_SHARED_POINTER
-	#define SHARED_POINTER
-	#define SHARED_PTR(CLASS, ptr) shared_ptr<CLASS>(ptr)
-	#define GetPtr(ptr) ptr.get()
+#define SHARED_POINTER
+#define SHARED_PTR(CLASS, ptr) shared_ptr<CLASS>(ptr)
+#define GetPtr(ptr) ptr.get()
 
 #else
-	#define SHARED_PTR(CLASS, ptr) ptr
-	#define GetPtr(ptr) ptr
+#define SHARED_PTR(CLASS, ptr) ptr
+#define GetPtr(ptr) ptr
 #endif //SHARED_POINTER
 
 #endif //_SYSINFO_H__
