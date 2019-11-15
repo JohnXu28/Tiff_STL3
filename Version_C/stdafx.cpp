31b4bd44a58fcf6367de29e25b56eb97e5a67c93
John_Xu <John_Xu@avision.com.tw>
2019-11-15 15:20:21 +0800

    Add, For Tiff_C X64 version

---
+++ Version_C/stdafx.cpp
@@ -0,0 +1,10 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Tiff_STL3.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in stdafx.h
+// and not in this file
+
+//Another info, test for git.
